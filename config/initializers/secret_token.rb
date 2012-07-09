e45988e5bc9d428d36a442ce3aa2c1b4ea3e3a5d
ArtemDordovsky <DordovskyArtem@gmail.com>
2012-07-09 18:23:11 +0300

    add product validates and test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot2::Application.config.secret_token = 'c85e1ade8199ffd32b006938e01f112e958dbffba62c198d4ac420a635363aa4c0ffe8e0635ca3aeccfa72a7f9330abd4d673b05da4fe73c6538bea9eac2e2a7'
---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Product.delete_all
+# . . .
+Product.create(:title => 'Programming Ruby 1.9',
+:description =>
+'
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.',
+:image_url => '/images/5300_2_70.jpg',
+:price => 49.50)
+# . . .
